8932eb9324bbd829de38334ecb846400940cf357
kanginhan <hunt2433@naver.com>
2020-04-15 16:16:16 +0900

    Updates

---
+++ precache-manifest.995e8faac3c701fff78b81be4ac4cc9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31dfb61a36fe75db1dbb40227926239",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb1c8aabe43359e81467",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1f1ea7721e74746ed0d9",
+    "url": "/static/css/main.29eed905.chunk.css"
+  },
+  {
+    "revision": "eb1c8aabe43359e81467",
+    "url": "/static/js/2.ce8985d7.chunk.js"
+  },
+  {
+    "revision": "260b7348d30ba1010f3469eb42d55815",
+    "url": "/static/js/2.ce8985d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f1ea7721e74746ed0d9",
+    "url": "/static/js/main.56e4ce69.chunk.js"
+  },
+  {
+    "revision": "b1078cff946660b45950",
+    "url": "/static/js/runtime-main.3ea2f64b.js"
+  }
+]);
