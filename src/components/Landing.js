c019587e93979abb4ae178ec212bb918cd2bafd9
kanginhan <hunt2433@naver.com>
2020-04-26 16:02:38 +0900

    메일 스타일 수정

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -182,6 +182,7 @@ const MailTo = styled.a`
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-top: 0.5rem;
 `;
 
 const goChannel = (history, channel) => {
@@ -384,6 +385,7 @@ const Landing = ({ history }) => {
         <span style={{ fontWeight: 600 }}>브로콜리, Wise Cow, 로봇</span>
         <br />
         <MailTo href="mailto:wavupme@gmail.com">
+          contact: 
           <i className="material-icons" style={{margin: "0 0.5rem", fontSize: "1rem"}}>mail_outline</i>wavupme@gmail.com
         </MailTo>
       </Footer>
