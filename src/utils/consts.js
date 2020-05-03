d513218395c28e1a4a24e4a429d531139b4576ec
kanginhan <hunt2433@naver.com>
2020-05-03 22:53:51 +0900

    api 경로변경

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,6 +1,7 @@
 export const URL_ROUTER_NAME = "";
 
-const apiBase = "http://localhost/";
+// const apiBase = "http://localhost/";
+const apiBase = "https://api.wavup.me/";
 export const API_URL = {
   getChannels: apiBase + "getChannels/",
 };
